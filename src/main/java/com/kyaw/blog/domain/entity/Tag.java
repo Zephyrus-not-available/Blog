0581d4a20d9468ff8cf6a63c0a0dd61a42ba08cf
Kyaw Hlaing Soe <kyawhlaingsoe572@gmail.com>
2026-02-12 15:12:10 +0630

    add many-to-many relationship between Post and Tag entities

--- src/main/java/com/kyaw/blog/domain/entity/Tag.java
+++ src/main/java/com/kyaw/blog/domain/entity/Tag.java
@@ -3,7 +3,9 @@ package com.kyaw.blog.domain.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -21,6 +23,9 @@ public class Tag {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "tags", fetch = FetchType.LAZY)
+    private Set<Post> posts = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
