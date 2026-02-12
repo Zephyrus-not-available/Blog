2b6e9e42cd3c9713284e478da7d8cf0d89500100
Kyaw Hlaing Soe <kyawhlaingsoe572@gmail.com>
2026-02-12 15:20:47 +0630

    add many-to-many relationship between Post and Tag entities

---
+++ src/main/java/com/kyaw/blog/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.kyaw.blog.repository;
+
+import com.kyaw.blog.domain.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID> {
+}
---
+++ src/main/java/com/kyaw/blog/repository/TagRepository.java
@@ -0,0 +1,11 @@
+package com.kyaw.blog.repository;
+
+import com.kyaw.blog.domain.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+}
