5f23f8975fbfc1400ca508d66088c853b0208c8f
Kyaw Hlaing Soe <kyawhlaingsoe572@gmail.com>
2026-02-15 20:58:12 +0630

    add JWT authentication: implement AuthServiceImpl for user login and token generation with configurable secret key

---
+++ src/main/java/com/kyaw/blog/service/impl/AuthServiceImpl.java
@@ -0,0 +1,54 @@
+package com.kyaw.blog.service.impl;
+
+import com.kyaw.blog.service.AuthService;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class AuthServiceImpl implements AuthService {
+
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationManager authenticationManager;
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final long jwtExpirationInMs = 8640000000L; // 1 hour
+
+    @Override
+    public UserDetails userLogin(String email, String password) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(email, password)
+        );
+        return userDetailsService.loadUserByUsername(email);
+    }
+
+    @Override
+    public String generateToken(UserDetails userDetails) {
+        Map<String,Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
+                .signWith(getSecretKey(),SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    private Key getSecretKey() {
+        byte[] keyBytes = secretKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
